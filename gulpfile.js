1ae009c189fd88e39ee3b315c1ab4a81a1f59048
sIri <bljuzgavic.22@gmail.com>
2016-11-11 09:20:38 +0100

    gulp setup working + the forgotten gitignore
    made sure that during the development it won't minify or combine any
    files to have quick browser refreshes and style injections
    added in all the dependencies so I can squeeze them all in one file per
    type(except for the fonts)

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp 		= require("gulp"),
+	
+	haml 		= require("gulp-haml-coffee"),
+	stylus 		= require("gulp-stylus"),
+
+	autoprefix	= require("gulp-autoprefixer"),
+	uglify 		= require("gulp-uglify"),
+	cssnano 	= require("gulp-cssnano"),
+
+	del 		= require("del"),
+	gulpIf 		= require("gulp-if"),
+	useref 		= require("gulp-useref"),
+	runSequence = require("run-sequence"),
+	browserSync = require("browser-sync");
+
+// Environment
+const
+	PRODUCTION 	= false,
+	DEV_DIR 	= "src",
+	PROD_DIR 	= "dist";
+var server 		= PRODUCTION ? PROD_DIR : DEV_DIR;
+
+// Build task should be run before running the default task
+gulp.task("build", function(callback) {
+	runSequence("clean", ["fonts", "stylus", "haml"], "useref", callback);
+});
+
+gulp.task("build--dev", function(callback) {
+	runSequence(["stylus", "haml"], callback);
+});
+
+
+gulp.task("default", function(callback) {
+	runSequence("browserSync", PRODUCTION ? "watch" : "watch--dev", callback);
+});
+
+
+gulp.task("browserSync", function() {
+	browserSync({
+		server: server
+	});
+});
+
+// Combining styles and scripts into one file each and minifying
+gulp.task("useref", function() {
+	return gulp.src(DEV_DIR + "/*.html")
+	.pipe(useref())
+	.pipe(gulpIf("*.js", uglify()))
+	.pipe(gulpIf("*.css", cssnano()))
+	.pipe(gulp.dest(PROD_DIR))
+	.pipe(browserSync.reload);
+});
+
+// Compile
+gulp.task("haml", function() {
+	return gulp.src(DEV_DIR + "/haml/**/*.haml")
+	.pipe(haml())
+	.pipe(gulp.dest(DEV_DIR))
+	.pipe(gulpIf(!PRODUCTION, browserSync.reload({stream: true})));
+});
+
+gulp.task("stylus", function() {
+	return gulp.src(DEV_DIR + "/stylus/**/*.styl")
+	.pipe(stylus())
+	.pipe(autoprefix())
+	.pipe(gulp.dest(DEV_DIR + "/css"))
+	.pipe(gulpIf(!PRODUCTION, browserSync.stream()));
+});
+
+// Copy the fonts over
+gulp.task("fonts", function() {
+  return gulp.src(DEV_DIR + "/fonts/**/*")
+    .pipe(gulp.dest(PROD_DIR + "/fonts"));
+});
+
+
+// Look for Haml, Stylus, and JS files for changes
+gulp.task("watch", function() {
+  gulp.watch(DEV_DIR + "/haml/**/*.haml", function() {
+  	runSequence("haml", "useref");
+  });
+  gulp.watch(DEV_DIR + "/stylus/**/*.styl", function() {
+  	runSequence("stylus", "useref");
+  });
+  gulp.watch(DEV_DIR + "/js/**/*.js", "useref");
+});
+
+gulp.task("watch--dev", function() {
+  gulp.watch(DEV_DIR + "/haml/**/*.haml", ["haml"]);
+  gulp.watch(DEV_DIR + "/stylus/**/*.styl", ["stylus"]);
+  gulp.watch(DEV_DIR + "/js/**/*.js").on("change", browserSync.reload);
+});
+
+
+// Clean up dist
+gulp.task("clean", function() {
+	return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function() {
+  var scrolling = false;
+  
+  function determineScroll(event, hash) {
+    event.preventDefault();
+    
+    if(scrolling) {
+      return;
+    } else {
+      moveMe(hash);
+    }
+  }
+  
+  function moveMe(hash) {
+    scrolling = true;
+    
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1500, $.bez([0.25,-0.20,0.25,1.20]), function() {
+     
+      window.location.hash = hash;
+      scrolling = false;
+    });
+  }
+  
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $("nav > .wrapper").addClass("sticky");
+    } else {
+      $("nav > .wrapper").removeClass("sticky");
+    }
+  });
+  
+  $(".navbar-nav a").click(function(event) {
+    determineScroll(event, this.hash);
+  });
+  
+  $("#intro button").click(function(event) {
+    determineScroll(event, "#about");
+  });
+  
+  setTimeout(function() {
+    $(".background").css("opacity", "1");
+  }, 200);
+});
